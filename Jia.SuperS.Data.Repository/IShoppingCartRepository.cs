cc585cb4895dc1a9ec8391e6f7175df4bb68984e
Jia <geogao07@gmail.com>
2018-08-22 17:07:46 -0400

    First project

---
+++ Jia.SuperS.Data.Repository/IShoppingCartRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jia.SuperS.Data.Model;
+
+namespace Jia.SuperS.Data.Repository
+{
+    public interface IShoppingCartRepository
+    {
+        ShoppingCart GetById(int id);
+        ShoppingCart Create();
+        ShoppingCart Update(int id, ShoppingCart newValue);
+    }
+}
